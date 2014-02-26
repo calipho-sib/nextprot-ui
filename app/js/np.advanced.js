218a69d2da1c8c8d207207371e37bea321882261
Daniel <ddtxra@gmail.com>
2014-02-26 13:52:42 +0100

    adding json file data

--- app/js/np.advanced.js
+++ app/js/np.advanced.js
@@ -15,6 +15,7 @@ AdvancedSearchModule.config([
 
 AdvancedSearchModule.controller('AdvancedCtrl', [
     '$resource',
+    '$http',
     '$scope',
     '$rootScope',
     '$location',
@@ -23,19 +24,30 @@ AdvancedSearchModule.controller('AdvancedCtrl', [
     'Search',
     'ProteinListService',
     'Tools',
-    function ($resource, $scope, $rootScope, $location, $routeParams, $route, Search, ProteinListService, Tools) {
+    function ($resource, $http, $scope, $rootScope, $location, $routeParams, $route, Search, ProteinListService, Tools) {
         $scope.currentStory;
         $scope.setCurrentStory = function(story) {
             $scope.currentStory = story;
         };
+
+        $http.get('queries/queries.json').success(function(data) {
+            $scope.stories = data;
+        });
+
         $scope.stories = [
 
-            {title: 'SPARQL Union', description: 'SELECT * WHERE { {?union_sparql_union :isoform/:function/:evidence/rdf:type :IEA} UNION {?union_sparql_union :isoform/:function/:evidence/rdf:type :ISS}}'},
-            {title: 'Story 01', description: 'Description pending.'},
-            {title: 'Story 02', description: 'Description pending.'},
-            {title: 'Story 03', description: 'Description pending.'},
-            {title: 'Story 04', description: 'Description pending.'},
-            {title: 'Story 05', description: 'Description pending.'}
+            {title: 'has transmembrane regions and ...', author: 'The NextProt team', sparql:
+                'SELECT ?entry WHERE { \n '+
+                '  ?entry  :isoform/:hasTransmembrane ?statement \n'+
+                    '} GROUP BY ?entry HAVING(count(?statement)>=2) \n',
+                description: ' you can replace the tansmembrane by any... like homospa'
+
+            },
+            {title: 'Story 01', sparql: 'Description pending.'},
+            {title: 'Story 02', sparql: 'Description pending.'},
+            {title: 'Story 03', sparql: 'Description pending.'},
+            {title: 'Story 04', sparql: 'Description pending.'},
+            {title: 'Story 05', sparql: 'Description pending.'}
         ];
     }
 
