24781365c3e444402c10ebe74818c0020d1a0bda
olivier <olivier.evalet@isb-sib.ch>
2014-12-02 10:32:40 +0100

    schublin release

--- app/js/user/np.user.protein.lists.js
+++ app/js/user/np.user.protein.lists.js
@@ -1,225 +1,201 @@
 (function (angular, undefined) {'use strict';
 
 angular.module('np.user.protein.lists', [
-		'np.user.protein.lists.service',
-    'np.user.protein.lists.ui',
-    'np.user.protein.lists.upload',
-    'np.user.protein.lists.upload.ui',
-    'np.flash'
+	'np.user.protein.lists.service',
+	'np.user.protein.lists.ui',
+	'np.flash'
 ])
 
-
+//
+// configure this module
 .config([
 	'$routeProvider',
-  '$locationProvider',
-  '$httpProvider',
-  function($routeProvider, $locationProvider, $httpProvider) {
-  	$routeProvider
-      	.when('/user/protein/lists', { templateUrl: 'partials/user/user-protein-lists.html'})
-        .when('/user/protein/lists/create', { templateUrl: 'partials/user/user-protein-lists-create.html'})
-  }
+	'$locationProvider',
+	'$httpProvider',
+	function($routeProvider, $locationProvider, $httpProvider) {
+		$routeProvider
+	    	.when('/user/protein/lists', { templateUrl: 'partials/user/user-protein-lists.html'})
+	      .when('/user/protein/lists/create', { templateUrl: 'partials/user/user-protein-lists-create.html'})
+	}
 ])
 
 
-.controller('ListCtrl', [
-	'$resource',
-	'$scope',
-	'$rootScope',
-	'$location',
-	'$routeParams',
-	'$route',
-	'Search',
-	'ProteinList',
-  'user',
-	function($resource, $scope, $rootScope, $location, $routeParams, $route, Search, ProteinList, user) {
-		$scope.ProteinList = ProteinList;
-		$scope.showCombine = false;
-		$scope.combineDisabled = true;
-		$scope.selected = {};
-		$scope.modal = { options: { edit: { title: 'Edit' }, create: { title: 'Create'} }, type:'create' };
-		$scope.lists=[]
-		$scope.operators = ["AND", "OR", "NOT_IN"];
-		$scope.combination = { first: null, op: $scope.operators[0], second: null};
-		$scope.options = { 
-			first: $scope.lists, 
-			second: $scope.lists 
-		}
+.controller('ListCtrl',ListCtrl)
+.controller('ListCreateCtrl',ListCreateCtrl);
 
-		ProteinList.getByUsername(user, function(data) {
-			$scope.lists = data.lists;
-			$scope.initCombination();
+//
+// Controller
+ListCtrl.$inject=['$resource','$scope','$rootScope','$location','$routeParams','$route','Search','ProteinList','user'];
+function ListCtrl($resource, $scope, $rootScope, $location, $routeParams, $route, Search, ProteinList, user) {
+	$scope.ProteinList = ProteinList;
+	$scope.showCombine = false;
+	$scope.combineDisabled = true;
+	$scope.selected = {};
+	$scope.modal = { options: { edit: { title: 'Edit' }, create: { title: 'Create'} }, type:'create' };
+	$scope.lists=[]
+	$scope.operators = ["AND", "OR", "NOT_IN"];
+	$scope.combination = { first: null, op: $scope.operators[0], second: null};
+	$scope.options = { 
+		first: $scope.lists, 
+		second: $scope.lists 
+	}
+
+	ProteinList.getByUsername(user, function(data) {
+		$scope.lists = data.lists;
+		$scope.initCombination();
+	});
+
+
+
+	//
+	// TODO what it mean?
+	$scope.initCombination = function() {
+
+
+		$scope.$watch('combination.first', function(newVal, oldVal) {
+			$scope.options.second = $scope.lists.slice(0);
+
+			var index = $scope.options.second.indexOf(newVal);
+			if(index > -1)
+				$scope.options.second.splice(index, 1);
 		});
 
 
+		$scope.$watch('combination.second', function(newVal, oldVal) {
+			$scope.options.first = $scope.lists.slice(0);
 
-		//
-		// TODO what it mean?
-		$scope.initCombination = function() {
+			var index = $scope.options.first.indexOf(newVal);
 
-
-			$scope.$watch('combination.first', function(newVal, oldVal) {
-				$scope.options.second = $scope.lists.slice(0);
-
-				var index = $scope.options.second.indexOf(newVal);
-				if(index > -1)
-					$scope.options.second.splice(index, 1);
-			});
+			if(index > -1)
+				$scope.options.first.splice(index, 1);
+		});
+	};
 
 
-			$scope.$watch('combination.second', function(newVal, oldVal) {
-				$scope.options.first = $scope.lists.slice(0);
+	$scope.modalDissmiss=function(){
 
-				var index = $scope.options.first.indexOf(newVal);
-
-				if(index > -1)
-					$scope.options.first.splice(index, 1);
-			});
-		};
-
-
-		$scope.modalDissmiss=function(){
-
-		}
-
-		$scope.switchCombine = function() {
-			var temp = $scope.combination.first;
-			$scope.combination.first = $scope.combination.second;
-			$scope.combination.second = temp;
-		};
-
-		$scope.launchModal = function(elem, action) {
-			$scope.selected = {};
-			if(action == 'edit') {
-				$scope.selected = $scope.lists[elem];
-				angular.extend($scope.selected, {index:elem});
-			}
-			angular.extend($scope.modal, { type: action});
-		};
-
-		$scope.saveModal = function(dismiss) {
-			if($scope.modal.type == 'edit') {
-				angular.extend($scope.lists[$scope.selected.index], $scope.selected);
-
-				var list = {
-					id: $scope.selected.id,
-					name: $scope.selected.name,
-					description: $scope.selected.description
-				};
-
-				ProteinList.update(user, list);
-			} else if($scope.modal.type == 'create') {
-				var newList = { name: $scope.selected.name, description: $scope.selected.description };
-
-				ProteinList.combine(
-                    user,
-					newList,
-					$scope.combination.first.name,
-					$scope.combination.second.name,
-					$scope.combination.op,
-					function(elem) {
-						//
-						// TODO why this is not always an array?
-						elem.accessions=elem.accessions.length
-						$scope.lists.push(elem)
-						$scope.options.first=$scope.options.second=$scope.lists
-						$log.info($scope.lists, $scope.options.first)
-					});
-
-			}
-			
-		};
-
-		$scope.delete = function(index) {
-			ProteinList.delete(user, $scope.lists[index].id);
-			$scope.lists.splice(index, 1);
-
-			$scope.options.first=$scope.options.second=$scope.lists
-		}
-
-
-		// TODO deprecated
-		$scope.buildQuery = function(accessions) {
-			return "id:" + (accessions.length > 1 ? "(" + accessions.join(" ") + ")" : accessions[0]);
-		};
 	}
-])
+
+	$scope.switchCombine = function() {
+		var temp = $scope.combination.first;
+		$scope.combination.first = $scope.combination.second;
+		$scope.combination.second = temp;
+	};
+
+	$scope.launchModal = function(elem, action) {
+		$scope.selected = {};
+		if(action == 'edit') {
+			$scope.selected = $scope.lists[elem];
+			angular.extend($scope.selected, {index:elem});
+		}
+		angular.extend($scope.modal, { type: action});
+	};
+
+	$scope.saveModal = function(dismiss) {
+		if($scope.modal.type == 'edit') {
+			angular.extend($scope.lists[$scope.selected.index], $scope.selected);
+
+			var list = {
+				id: $scope.selected.id,
+				name: $scope.selected.name,
+				description: $scope.selected.description
+			};
+
+			ProteinList.update(user, list);
+		} else if($scope.modal.type == 'create') {
+			var newList = { name: $scope.selected.name, description: $scope.selected.description };
+
+			ProteinList.combine(
+                  user,
+				newList,
+				$scope.combination.first.name,
+				$scope.combination.second.name,
+				$scope.combination.op,
+				function(elem) {
+					//
+					// TODO why this is not always an array?
+					elem.accessions=elem.accessions.length
+					$scope.lists.push(elem)
+					$scope.options.first=$scope.options.second=$scope.lists
+					$log.info($scope.lists, $scope.options.first)
+				});
+
+		}
+		
+	};
+
+	$scope.delete = function(index) {
+		ProteinList.delete(user, $scope.lists[index].id);
+		$scope.lists.splice(index, 1);
+
+		$scope.options.first=$scope.options.second=$scope.lists
+	}
+
+}
 
 
-.controller('ListCreateCtrl', [
-	'$resource',
-	'$scope',
-	'$rootScope',
-	'$routeParams',
-	'$location',
-	'ProteinList',
-  'user',
-	'UploadListService',
-	'flash',
-	'$log',
-	function($resource, $scope, $rootScope, $routeParams, $location, ProteinList, user, UploadListService, flash, $log) {
+ListCreateCtrl.$inject=['$resource','$scope','$rootScope','$routeParams','$location','ProteinList','user','UploadListService','flash','$log']
+function ListCreateCtrl($resource, $scope, $rootScope, $routeParams, $location, ProteinList, user, UploadListService, flash, $log) {
+	$scope.inputAccessions = "";
+	$scope.listName = "";
+	$scope.files = [];
+	var selectedFiles = [];
 
-		$scope.inputAccessions = "";
-		$scope.listName = "";
-		$scope.files = [];
-		var selectedFiles = [];
-
-		$scope.$watch('files', function (newValue, oldValue) {
-	        // Only act when our property has changed.
-	        if (newValue != oldValue) {
-	        	selectedFiles = $scope.files;
+	$scope.$watch('files', function (newValue, oldValue) {
+        // Only act when our property has changed.
+        if (newValue != oldValue) {
+        	selectedFiles = $scope.files;
 
 
-	            //$log.info('Controller: $scope.files changed. Start upload.');
-	            //for (var i = 0, length = $scope.files.length; i < length; i++) {
-	                // Hand file off to uploadService.
-	              //  UploadListService.send($scope.files[i]);
-	            //}
-	        }
-	    }, true);
+            //$log.info('Controller: $scope.files changed. Start upload.');
+            //for (var i = 0, length = $scope.files.length; i < length; i++) {
+                // Hand file off to uploadService.
+              //  UploadListService.send($scope.files[i]);
+            //}
+        }
+    }, true);
 
-		$rootScope.$on('upload:loadstart', function () {
-	        $log.info('Controller: on `loadstart`');
-	    });
+	$rootScope.$on('upload:loadstart', function () {
+        $log.info('Controller: on `loadstart`');
+    });
 
-	    $rootScope.$on('upload:error', function () {
-	        $log.info('Controller: on `error`');
-	    });
+    $rootScope.$on('upload:error', function () {
+        $log.info('Controller: on `error`');
+    });
 
-	    $scope.createList = function(listName) {
+    $scope.createList = function(listName) {
 
-	    	if($scope.inputAccessions.length > 0) {
-	    		var accessions = $scope.inputAccessions.split("\n");
-	    		var list = { name: $scope.listName, accessions: accessions};
+    	if($scope.inputAccessions.length > 0) {
+    		var accessions = $scope.inputAccessions.split("\n");
+    		var list = { name: $scope.listName, accessions: accessions};
 
-	    		ProteinList.create(user, list, function(data) {
+console.log(user)
+    		ProteinList.create(user, list, function(data) {
+				if(data.error) flash('alert-warning', data.error);
+				else {
+					flash('alert-info', "List "+list.name+" created.");
+					$location.path('/user/protein/lists');
+				}
+    		});
+    	} else {
+    		ProteinList.create(user, {
+                  name: $scope.listName, description: $scope.listDescription, accessions: []
+              }, function(newList) {
+
+    			for(var i=0; i<selectedFiles.length; i++)
+    			UploadListService.send(newList.id, selectedFiles[i], function(data) {
 					if(data.error) flash('alert-warning', data.error);
 					else {
-						flash('alert-info', "List "+list.name+" created.");
+						flash('alert-info', "List "+$scope.listName+" created.");
 						$location.path('/user/protein/lists');
 					}
-	    		});
-	    	} else {
-	    		ProteinList.create(user, {
-                    name: $scope.listName, description: $scope.listDescription, accessions: []
-                }, function(newList) {
+    			});
+    		});
 
-	    			for(var i=0; i<selectedFiles.length; i++)
-	    			UploadListService.send(newList.id, selectedFiles[i], function(data) {
-						if(data.error) flash('alert-warning', data.error);
-						else {
-							flash('alert-info', "List "+$scope.listName+" created.");
-							$location.path('/user/protein/lists');
-						}
-	    			});
-	    		});
+    	}
+    }
 
-	    	}
-	    }
-
-	}
-
-
-]);
+}
 
 })(angular);
 
--- app/js/user/np.user.protein.lists.ui.js
+++ app/js/user/np.user.protein.lists.ui.js
@@ -30,7 +30,27 @@ angular.module('np.user.protein.lists.ui', [])
 			});
 		}
 	};
-}]);
+}])
+
+
+.directive('fileChange', function() {
+	 var linker = function ($scope, element, attributes) {
+	        // onChange, push the files to $scope.files.
+	        element.bind('change', function (event) {
+	            var files = event.target.files;
+	            $scope.$apply(function () {
+	                for (var i = 0, length = files.length; i < length; i++) {
+	                    $scope.files.push(files[i]);
+	                }
+	            });
+	        });
+	    };
+
+	    return {
+	        restrict: 'A',
+	        link: linker
+	    };
+});
 
 
 })(angular);
