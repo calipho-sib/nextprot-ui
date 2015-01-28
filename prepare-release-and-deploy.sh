8f2fc03cee1eb5ce60ef70ec230f8b9f128640cf
fnikitin <frederic.nikitin@gmail.com>
2015-01-28 16:45:23 +0100

    test release v0.0.5

--- prepare-release-and-deploy.sh
+++ prepare-release-and-deploy.sh
@@ -6,7 +6,7 @@
 # This script is triggered in a jenkins job each time a push is done on branch master.
 
 # start debug mode
-set -x
+#set -x
 
 # return the next release version to prepare on master
 getNextReleaseVersion () {
@@ -15,41 +15,45 @@ getNextReleaseVersion () {
     if [ ! -f "pom.xml" ]; then
 
         echo "pom.xml was not found: not a maven project"
-        exit 1
+        return 1
     fi
 
     # get develop version (http://stackoverflow.com/questions/3545292/how-to-get-maven-project-version-to-the-bash-command-line)
-    devVersion=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)'`
+    DEV_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)'`
+    RELEASE_NAME=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.name | grep -Ev '(^\[|Download\w+:)'`
 
-    if [[ ! ${devVersion} =~ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT ]]; then
+    if [[ ! ${DEV_VERSION} =~ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT ]]; then
 
-        devName=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.name | grep -Ev '(^\[|Download\w+:)'`
-
-        echo "cannot release ${devName} v${devVersion}: not a snapshot (develop) version"
-        exit 2
+        echo "cannot release ${RELEASE_NAME} v${DEV_VERSION}: not a snapshot (develop) version"
+        return 2
     fi
 
-    # return release version
-    echo ${devVersion%-*}
+    RELEASE_VERSION=${DEV_VERSION%-*}
+
+    return 0
 }
 
 # change branch to master
-git checkout master || exit 3
+git checkout master
 
 # get release version to prepare
-RELEASE_VERSION=$(getNextReleaseVersion)
+if ! getNextReleaseVersion RELEASE_VERSION; then
+    exit 1
+fi
+
+echo preparing ${RELEASE_NAME} v${RELEASE_VERSION}...
 
 # prepare new version
-mvn versions:set -DnewVersion=${RELEASE_VERSION} -DgenerateBackupPoms=false || exit 4
+mvn versions:set -DnewVersion=${RELEASE_VERSION} -DgenerateBackupPoms=false || exit 2
 
 git add pom.xml
-git commit -m "New release version ${RELEASE_VERSION}" # returns -1 if nothing to commit
+git commit -m "New release version ${RELEASE_VERSION}"
 
 # create a new release tag
-git tag -a v${RELEASE_VERSION} -m "tag v${RELEASE_VERSION}" || exit 5
-git push origin master --tags || exit 6
+git tag -a v${RELEASE_VERSION} -m "tag v${RELEASE_VERSION}" || exit 3
+git push origin master --tags || exit 4
 
 # deploy on nexus
-mvn clean deploy || exit 7
+mvn clean deploy || exit 5
 
 exit 0
