2add9781a0476aa8fe50454f8246e45ab47b9cad
Daniel <ddtxra@gmail.com>
2015-02-04 14:24:28 +0100

    just indenting / formating

--- app/js/np.js
+++ app/js/np.js
@@ -1,37 +1,38 @@
-(function (angular, undefined) {'use strict';
+(function (angular, undefined) {
+    'use strict';
 
 //Declare application level module which depends on additional filters and services (most of them are custom)
-var App = angular.module('np', [
-    'ngSanitize',
-    'ngResource',
-    'ngRoute',
-    'ngAnimate',
-    'ngCookies',
-    'npHelp',
-    '$strap.directives',
-    'np.flash',
-    'np.config',
-    'np.user',
-    'np.cart',
-    'np.user.protein.lists',
-    'np.search',
-    'np.export',
-    'ui.codemirror',
-    'auth0', 'angular-storage', 'angular-jwt'
-]).config(configApplication)
-  .factory('errorInterceptor',errorInterceptor)
-  .run(runApplication);
+    var App = angular.module('np', [
+        'ngSanitize',
+        'ngResource',
+        'ngRoute',
+        'ngAnimate',
+        'ngCookies',
+        'npHelp',
+        '$strap.directives',
+        'np.flash',
+        'np.config',
+        'np.user',
+        'np.cart',
+        'np.user.protein.lists',
+        'np.search',
+        'np.export',
+        'ui.codemirror',
+        'auth0', 'angular-storage', 'angular-jwt'
+    ]).config(configApplication)
+        .factory('errorInterceptor', errorInterceptor)
+        .run(runApplication);
 
 
     //Environment that should be set from outside //TODO should replace this using GRUNT
     var nxEnvironment = "NX_ENV"; //env can be replaced, by dev, alpha or pro
     var apiBase = "http://localhost:8080/nextprot-api-web"; //default
 
-    if(nxEnvironment.indexOf("NX_") == -1) // means an environment has been set, sed command has done some magic tricks
+    if (nxEnvironment.indexOf("NX_") == -1) // means an environment has been set, sed command has done some magic tricks
     {
-        apiBase='http://' + nxEnvironment.toLowerCase() +'-api.nextprot.org';
-        if(nxEnvironment.toLowerCase() ===  "pro"){
-            apiBase='https://api.nextprot.org'; // Don't forget https!
+        apiBase = 'http://' + nxEnvironment.toLowerCase() + '-api.nextprot.org';
+        if (nxEnvironment.toLowerCase() === "pro") {
+            apiBase = 'https://api.nextprot.org'; // Don't forget https!
         }
     }
     // main application settings
@@ -44,66 +45,65 @@ var App = angular.module('np', [
     })
 
 
-//
-// init application components
-runApplication.$inject=['$log','gitHubContent','npSettings']
-function runApplication($log,gitHubContent,npSettings) {
-    gitHubContent.initialize({
-            baseUrl:"https://api.nextprot.org",
-            helpPath:'/rdf/help/type/all.json',
-            helpTitle:'Main truc',
-            root:'',                                                            // specify a URI prefix
-            githubRepo:'calipho-sib/nextprot-docs',
-            githubToken:npSettings.githubToken
-    });
-};
-
-//
-// config application $route, $location and $http services.
-configApplication.$inject=['$routeProvider','$locationProvider','$httpProvider','authProvider','npSettings', 'jwtInterceptorProvider'];
-function configApplication( $routeProvider,  $locationProvider, $httpProvider, authProvider, npSettings, jwtInterceptorProvider) {
-    authProvider.init({
-        clientID: npSettings.auth0_cliendId,
-        callbackURL: npSettings.callback, 
-        domain: 'nextprot.auth0.com',
-        icon: 'img/np.png'
-    })
+    // init application components
+    runApplication.$inject = ['$log', 'gitHubContent', 'npSettings']
+    function runApplication($log, gitHubContent, npSettings) {
+        gitHubContent.initialize({
+            baseUrl: "https://api.nextprot.org",
+            helpPath: '/rdf/help/type/all.json',
+            helpTitle: 'Main truc',
+            root: '',                                                            // specify a URI prefix
+            githubRepo: 'calipho-sib/nextprot-docs',
+            githubToken: npSettings.githubToken
+        });
+    };
 
 
-    jwtInterceptorProvider.tokenGetter = ['store', function(store) {
-        // Return the saved token
-        return store.get('token');
-    }];
-    $httpProvider.interceptors.push('jwtInterceptor');
+    // config application $route, $location and $http services.
+    configApplication.$inject = ['$routeProvider', '$locationProvider', '$httpProvider', 'authProvider', 'npSettings', 'jwtInterceptorProvider'];
+    function configApplication($routeProvider, $locationProvider, $httpProvider, authProvider, npSettings, jwtInterceptorProvider) {
+        authProvider.init({
+            clientID: npSettings.auth0_cliendId,
+            callbackURL: npSettings.callback,
+            domain: 'nextprot.auth0.com',
+            icon: 'img/np.png'
+        })
 
-    $httpProvider.interceptors.push('errorInterceptor');
-    $httpProvider.defaults.headers.common.Accept= 'application/json'
+
+        jwtInterceptorProvider.tokenGetter = ['store', function (store) {
+            // Return the saved token
+            return store.get('token');
+        }];
+        $httpProvider.interceptors.push('jwtInterceptor');
+
+        $httpProvider.interceptors.push('errorInterceptor');
+        $httpProvider.defaults.headers.common.Accept = 'application/json'
 
 
-    // List of routes of the application
-    $routeProvider
-        .when('/', {title: 'welcome to nextprot', templateUrl: '/partials/welcome.html'})
-        .when('/doc', {title: 'welcome to nextprot', templateUrl: '/partials/doc/doc.html'})
-        .when('/doc/entity/:entity', {title: 'welcome to nextprot', templateUrl: '/partials/doc/help.html'})
-        .when('/pages/:article', {title: 'page', templateUrl: '/partials/doc/page.html'})
-        .when('/404', {title: '404', templateUrl: '/partials/errors/404.html'})
-    // Catch all
-    //.otherwise({redirectTo : '/404'});
+        // List of routes of the application
+        $routeProvider
+            .when('/', {title: 'welcome to nextprot', templateUrl: '/partials/welcome.html'})
+            .when('/doc', {title: 'welcome to nextprot', templateUrl: '/partials/doc/doc.html'})
+            .when('/doc/entity/:entity', {title: 'welcome to nextprot', templateUrl: '/partials/doc/help.html'})
+            .when('/pages/:article', {title: 'page', templateUrl: '/partials/doc/page.html'})
+            .when('/404', {title: '404', templateUrl: '/partials/errors/404.html'})
+        // Catch all
+        //.otherwise({redirectTo : '/404'});
 
-    // Without serve side support html5 must be disabled.
-    $locationProvider.html5Mode(true);
-    //$locationProvider.hashPrefix = '!';
-};
+        // Without serve side support html5 must be disabled.
+        $locationProvider.html5Mode(true);
+        //$locationProvider.hashPrefix = '!';
+    };
 
 
 // define default behavior for all http request
-errorInterceptor.$inject=['$q', '$rootScope', '$location', 'flash']
-function errorInterceptor($q, $rootScope, $location, flash) {
+    errorInterceptor.$inject = ['$q', '$rootScope', '$location', 'flash']
+    function errorInterceptor($q, $rootScope, $location, flash) {
         return {
             request: function (config) {
                 return config || $q.when(config);
             },
-            requestError: function(request){
+            requestError: function (request) {
                 return $q.reject(request);
             },
             response: function (response) {
@@ -119,22 +119,22 @@ function errorInterceptor($q, $rootScope, $location, flash) {
                     flash('alert-danger', "You are not authorized to access the resource. Please login or review your privileges.");
                     return;
                 }/*else if (status == 404) {
-                    flash('alert-danger', "URL not found");
-                    return;
-                } */
+                 flash('alert-danger', "URL not found");
+                 return;
+                 } */
                 else if (status >= 500) {
                     console.log(response)
-                    if(response.message){
+                    if (response.message) {
                         flash('alert-warning', response.message);
-                    }else if (response.data.message) {
+                    } else if (response.data.message) {
                         flash('alert-danger', response.data.message);
-                    } else 
+                    } else
                         flash('alert-danger', 'Some error occured' + " " + status + " " + response);
                 }
                 return $q.reject(response);
             }
         };
-};
+    };
 
 })(angular);
 
