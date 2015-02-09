d5a39c8fdf8e2a20229b3e81fd5bae5be7932209
Daniel <ddtxra@gmail.com>
2015-02-09 16:47:14 +0100

    adding echo and make sure that jetty has stopped and started

--- src/DeployNP2/deploy-api.sh
+++ src/DeployNP2/deploy-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+#set -x
 set -o errexit  # make your script exit when a command fails.
 set -o pipefail # prevents errors in a pipeline from being masked. If any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
 set -o nounset  # exit when your script tries to use undeclared variables.
@@ -11,11 +11,23 @@ set -o nounset  # exit when your script tries to use undeclared variables.
 function stop_jetty() {
   host=$1
   ssh npteam@$host /work/jetty/bin/jetty.sh stop
+  echo "Stopping jetty ..."
+  while [ ! -f /work/jetty/jetty.pid ]
+  do
+    sleep 1
+  done
+  echo "Jetty has stopped in $1"
 }
 
 function start_jetty() {
+  echo "Starting jetty ..."
   host=$1
-  ssh npteam@$host "/work/jetty/bin/jetty.sh start > /dev/null 2>&1 &"
+  ssh npteam@$host "/work/jetty/bin/jetty.sh jetty start > /dev/null 2>&1 &"
+  while [ grep STARTED /work/jetty/jetty.state ]
+  do
+    sleep 1
+  done
+  echo "Jetty has started in $1"
 }
 
 SRC_HOST=$1
@@ -27,6 +39,7 @@ time stop_jetty ${SRC_HOST}
 
 dirs="webapps cache repository"
 for dir in $dirs; do
+  echo "Copying directory $dir to ${TRG_HOST}"
   ssh npteam@${TRG_HOST} "rm -rf /work/jetty/${dir}.new"
   ssh npteam@${TRG_HOST} "mkdir /work/jetty/${dir}.new"
   ssh npteam@${SRC_HOST} "rsync -avz /work/jetty/${dir}/* npteam@${TRG_HOST}:/work/jetty/${dir}.new"
@@ -40,6 +53,7 @@ stop_jetty ${TRG_HOST}
 
 dirs="webapps cache repository"
 for dir in $dirs; do
+  echo "Backing up directory $dir in ${TRG_HOST}"
   ssh npteam@${TRG_HOST} "rm -rf /work/jetty/${dir}.bak"
   ssh npteam@${TRG_HOST} "mv /work/jetty/${dir} /work/jetty/${dir}.bak "
   ssh npteam@${TRG_HOST} "mv /work/jetty/${dir}.new /work/jetty/${dir} "
