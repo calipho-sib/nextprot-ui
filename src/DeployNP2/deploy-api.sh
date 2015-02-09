2f3caa5c018ccc39949be8a52f89d6a9d1a40668
Daniel <ddtxra@gmail.com>
2015-02-09 17:25:07 +0100

    Fixing missing ssh bash instruction

--- src/DeployNP2/deploy-api.sh
+++ src/DeployNP2/deploy-api.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-color='\e[1;34m'
-NC='\e[0m' # No Color
+
+color='\e[1;34m' # begin color
+_color='\e[0m'   # end Color
 
 #set -x
 set -o errexit  # make your script exit when a command fails.
@@ -8,43 +9,42 @@ set -o pipefail # prevents errors in a pipeline from being masked. If any comman
 set -o nounset  # exit when your script tries to use undeclared variables.
 #set -o xtrace   # trace what gets executed. Useful for debugging.
 
-
-
 function stop_jetty() {
   host=$1
+
+  if [ -f "ssh npteam@kant /work/jetty/jetty.pid" ]; then
+      echo -e "${color}Jetty is not running at $host ${_color}"
+      return 0
+  fi
+
   ssh npteam@$host /work/jetty/bin/jetty.sh stop
-  echo -e "{color}Stopping jetty ...{NC}"
-  while [ -f /work/jetty/jetty.pid ]
-  do
-    sleep 1
+  echo -e "${color}Stopping jetty ...${_color}"
+
+  while [ -f "ssh npteam@kant /work/jetty/jetty.pid" ]; do
+      sleep 1
   done
-  echo -e "{color}Jetty has stopped in $1{NC}"
+
+  echo -e "${color}Jetty has stopped at $host ${_color}"
 }
 
 function start_jetty() {
-  echo -e "{color}Starting jetty ...{NC}"
+  echo -e "${color}Starting jetty ...${_color}"
   host=$1
   ssh npteam@$host "/work/jetty/bin/jetty.sh jetty start > /dev/null 2>&1 &"
-  while true ; do
-    if ssh npteam@$host "grep STARTED /work/jetty/jetty.state"; then
-        break;
-    else
-        sleep 1
-    fi
+  while [ ! ssh npteam@$host "grep STARTED /work/jetty/jetty.state" ]; do
+      sleep 1
   done
-  echo -e "{color}Jetty has started in $1{NC}"
+  echo -e "${color}Jetty has started at $host ${_color}"
 }
 
 SRC_HOST=$1
 TRG_HOST=$2
 
-
-time stop_jetty ${SRC_HOST}
-
+stop_jetty ${SRC_HOST}
 
 dirs="webapps cache repository"
 for dir in $dirs; do
-  echo -e "{color}Copying directory $dir to ${TRG_HOST}{NC}"
+  echo -e "${color}Copying directory $dir to ${TRG_HOST}${_color}"
   ssh npteam@${TRG_HOST} "rm -rf /work/jetty/${dir}.new"
   ssh npteam@${TRG_HOST} "mkdir /work/jetty/${dir}.new"
   ssh npteam@${SRC_HOST} "rsync -auz /work/jetty/${dir}/* npteam@${TRG_HOST}:/work/jetty/${dir}.new"
@@ -52,18 +52,14 @@ done
 
 start_jetty ${SRC_HOST}
 
-
-
 stop_jetty ${TRG_HOST}
 
 dirs="webapps cache repository"
 for dir in $dirs; do
-  echo -e "{color}Backing up directory $dir in ${TRG_HOST}{NC}"
+  echo -e "${color}Backing up directory $dir in ${TRG_HOST}${_color}"
   ssh npteam@${TRG_HOST} "rm -rf /work/jetty/${dir}.bak"
   ssh npteam@${TRG_HOST} "mv /work/jetty/${dir} /work/jetty/${dir}.bak "
   ssh npteam@${TRG_HOST} "mv /work/jetty/${dir}.new /work/jetty/${dir} "
 done
 
 start_jetty ${TRG_HOST}
-
-
