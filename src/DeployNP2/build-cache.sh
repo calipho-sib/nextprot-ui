9a42cbf1e4dc27e886292ad29bb2eae639ba27e9
neXtProt Team <ndu@isb-sib.ch>
2015-02-11 17:26:42 +0100

    Update build-cache.sh

--- src/DeployNP2/build-cache.sh
+++ src/DeployNP2/build-cache.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-sh -c "wget http://build-api.nextprot.org/export/entries/all.xml &> /dev/null&"
+set -x
+for i in {1..22}
+do
+wget http://build-api.nextprot.org/export/entries/chromosome/$i.xml -O /dev/null
+done
+
+wget http://build-api.nextprot.org/export/entries/chromosome/MT.xml -O /dev/null
+wget http://build-api.nextprot.org/export/entries/chromosome/X.xml -O /dev/null
+wget http://build-api.nextprot.org/export/entries/chromosome/Y.xml -O /dev/null
+wget http://build-api.nextprot.org/export/entries/chromosome/unknown.xml -O /dev/null
+
+echo "Getting help"
+wget http://build-api.nextprot.org/rdf/help/type/all.json -O /dev/null
+set +x
