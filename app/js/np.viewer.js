897c376a36cbfd0e2d3dee2340e6176b7615e380
Daniel <ddtxra@gmail.com>
2015-02-27 16:44:10 +0100

    adding viewer widgets

--- app/js/np.viewer.js
+++ app/js/np.viewer.js
@@ -9,8 +9,10 @@
     viewerConfig.$inject=['$routeProvider'];
     function viewerConfig($routeProvider) {
         $routeProvider
-            .when('/viewer/:viewd', {title: 'welcome to nextprot', templateUrl: '/partials/viewer.html'})
+            .when('/entry/:entry/gist/:gistusr/:gistid', { templateUrl: '/partials/widget/viewer.html'})
             .when('/entry/:entry/:element', { templateUrl: '/partials/widget/viewer.html'})
+            .when('/entry/:entry/', { templateUrl: '/partials/widget/viewer.html'})
+
     }
 
 
@@ -18,11 +20,27 @@
     function ViewerCtrl($resource, $scope, $sce, $routeParams, config) {
         $scope.widgetEntry = null;
 
+        $scope.activePage = function(page) {
+          if($routeParams.element){
+            if(page === $routeParams.element) return 'active';
+          }else if($routeParams.gistusr && $routeParams.gistusr){
+            if(page === ($routeParams.gistusr + "/" + $routeParams.gistid)) return 'active';
+          }
+          else return '';
+        }
 
-        $scope.widgetURL = $sce.trustAsResourceUrl("http://www.nextprot.org/db/entry/" + $routeParams.entry + "/" + $routeParams.element);
         // update entity documentation on path change
         $scope.$on('$routeChangeSuccess', function(event, next, current) {
           $scope.widgetEntry = $routeParams.entry;
+          if($routeParams.gistusr && $routeParams.gistid){
+            $scope.widgetURL = $sce.trustAsResourceUrl("http://bl.ocks.org/" + $routeParams.gistusr + "/raw/" + $routeParams.gistid + "/?nxentry=" +  $routeParams.entry);
+          }else { //nextprot
+            if($routeParams.element){
+              $scope.widgetURL = $sce.trustAsResourceUrl("http://www.nextprot.org/db/entry/" + $routeParams.entry + "/" + $routeParams.element);
+            }else{
+              $scope.widgetURL = $sce.trustAsResourceUrl("http://www.nextprot.org/db/entry/" + $routeParams.entry);
+            }
+          }
         });
 
     }
