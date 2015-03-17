6a20f948f95e758f96842b6244200a1b334fc937
fnikitin <frederic.nikitin@gmail.com>
2015-03-17 15:51:26 +0100

    Replace addAll and removeAll to clipboard buttons with a checkbox

--- app/js/np.cart.service.js
+++ app/js/np.cart.service.js
@@ -12,7 +12,7 @@ CartService.factory('Cart', [
 
         var Cart = function () {
             elements = sessionStorage.cart ? this.getCartFromStorage().elements : [];
-        }
+        };
 
 
         // entry changes status ( selected => unselected ; unselected => selected; )
@@ -31,54 +31,53 @@ CartService.factory('Cart', [
             this.saveCartToStorage();
 
             return found;
-        }
+        };
 
 
         Cart.prototype.isInCart = function (docId) {
             return (_.indexOf(elements, docId) != -1);
-        }
+        };
 
         Cart.prototype.getCartFromStorage = function () {
             return angular.fromJson(sessionStorage.cart);
-        }
+        };
 
         Cart.prototype.saveCartToStorage = function () {
             sessionStorage.cart = angular.toJson({elements: elements});
-        }
+        };
 
         Cart.prototype.setCart = function (docIds) {
             elements = _.union(elements, docIds);
             this.saveCartToStorage();
-        }
+        };
 
         Cart.prototype.removeFromCart = function (docIds) {
             elements = _.difference(elements, docIds);
             this.saveCartToStorage();
-        }
+        };
 
         Cart.prototype.getCartSize = function () {
             return elements.length;
-        }
+        };
 
         Cart.prototype.emptyCart = function () {
             elements = [];
             this.saveCartToStorage();
-        }
+        };
 
         Cart.prototype.getElements = function () {
             return elements;
-        }
+        };
 
         Cart.prototype.inCart = function (id) {
             return elements.indexOf(id) != -1;
-        }
+        };
 
         Cart.prototype.remove = function (id) {
             if (elements.length == 1)
                 elements = [];
             else elements = elements.splice(id, 1);
-        }
-
+        };
 
         var cart = new Cart();
         return cart;
--- app/js/np.search.ui.js
+++ app/js/np.search.ui.js
@@ -393,9 +393,6 @@
             scope: true,
             // Require that "ngModel" directive be present for this directive to function correctly.
             require: '?ngModel',
-            // scope – A scope to be used by the directive (https://docs.angularjs.org/api/ng/type/$rootScope.Scope).
-            // element – An element the directive is bound to (https://docs.angularjs.org/api/ng/function/angular.element)
-            // attrs – A list of attributes associated with the element ().
             link: function (scope, element, attrs, modelCtrl) {
                 var proteinList = attrs.proteinList;
                 var property = attrs.property;
@@ -405,21 +402,21 @@
                     scope.$apply(function () {
                         var isChecked = element.prop('checked');
 
-                        // Set each child's selected property to the checkbox's checked property
-                        angular.forEach(scope.$eval(childList), function (child) {
-                            child[property] = isChecked;
+                        // Set each protein's selected property to the checkbox's checked property
+                        angular.forEach(scope.$eval(proteinList), function (protein) {
+                            protein[property] = isChecked;
                         });
                     });
                 });
 
                 // Watch the children for changes
-                scope.$watch(childList, function (newValue) {
+                scope.$watch(proteinList, function (newValue) {
                     var hasChecked = false;
                     var hasUnchecked = false;
 
                     // Loop through the children
-                    angular.forEach(newValue, function (child) {
-                        if (child[property]) {
+                    angular.forEach(newValue, function (protein) {
+                        if (protein[property]) {
                             hasChecked = true;
                         } else {
                             hasUnchecked = true;
