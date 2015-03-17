2c40769d71cc72e2206f7627d950467f871e0d56
fnikitin <frederic.nikitin@gmail.com>
2015-03-17 11:16:34 +0100

    Remove dependency to ng-truncate.js.  Truncate protein descriptions feature implemented in-house

--- app/js/np.search.ui.js
+++ app/js/np.search.ui.js
@@ -138,10 +138,10 @@
         return window.encodeURIComponent;
     });
 
-    SearchUI.filter('limit', function () {
+    /*SearchUI.filter('limit', function () {
         return function (value, max, wordwise, tail) {
             if (!value) return '';
-            if (!wordwise)wordwise = true;
+            if (!wordwise) wordwise = true;
 
             max = parseInt(max, 10);
             if (!max) return value;
@@ -156,6 +156,27 @@
             }
 
             return value + (tail || ' ...');
+        };
+    });*/
+
+    SearchUI.filter('head', function () {
+        return function (value, max, wordwise) {
+            if (!value) return '';
+            if (!wordwise) wordwise = true;
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value;
         };
     });
 
@@ -195,6 +216,20 @@
                     elm.context.text = "Show Abstract";
                 }
                 angular.element(attrs.npToggleAbstract).toggleClass("hide")
+            })
+        };
+    }]);
+
+    SearchUI.directive('npToggleMore', [function () {
+        return function (scope, elm, attrs) {
+            elm.click(function () {
+
+                if (elm.context.text.match(/more/)) {
+                    elm.context.text = "less";
+                } else if (elm.context.text.match(/less/)) {
+                    elm.context.text = "more";
+                }
+                angular.element(attrs.npToggleMore).toggleClass("hide")
             })
         };
     }]);
