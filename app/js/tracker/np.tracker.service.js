c21d1e68bdd2d93fda73637d56f1eef2666c2fb4
fnikitin <frederic.nikitin@gmail.com>
2015-04-21 12:03:45 +0200

    Change predicate to assign correct tracking id

--- app/js/tracker/np.tracker.service.js
+++ app/js/tracker/np.tracker.service.js
@@ -307,9 +307,9 @@ TrackingService.factory('Tracker', [
 
         function getTrackingId() {
 
-            console.log("trackingProduction: ", trackingProduction, (trackingProduction != 'NX_TRACKING_PROD'));
+            console.log("trackingProduction: ", trackingProduction);
 
-            var trackingId = (trackingProduction != 'NX_TRACKING_PROD') ? productionTrackingId : developTrackingId;
+            var trackingId = (trackingProduction == "true") ? productionTrackingId : developTrackingId;
 
             console.log('Tracking ids: develop:', developTrackingId, ', production:', productionTrackingId, ', current tracking:', trackingId);
 
