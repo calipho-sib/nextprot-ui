84b4a9d2ec909425afff6022f075ba570eee36b9
fnikitin <frederic.nikitin@gmail.com>
2015-09-08 16:54:59 +0200

    Force angular to check that $scope has changed to refresh the list of files in the view

--- app/js/user/np.user.protein.lists.ui.js
+++ app/js/user/np.user.protein.lists.ui.js
@@ -1,54 +1,57 @@
-(function (angular, undefined) {'use strict';
+(function (angular, undefined) {
+    'use strict';
 
-angular.module('np.user.protein.lists.ui', [])
+    angular.module('np.user.protein.lists.ui', [])
 
-.directive('fadeOnHover', ['$parse', '$timeout', function($parse, $timeout) {
-	return function(scope, element, attr) {
-		$timeout(function() {
-			var e = angular.element(attr['fadeOnHover']);
-			if(e.length) {
-				e.bind('mouseenter', function() {
-					element.fadeIn('fast');
-				}).bind('mouseleave', function() {
-					element.fadeOut('fast');
-				});
-			}
-		}, 60);
-		element.hide();
-	}
-}])
+        .directive('fadeOnHover', ['$parse', '$timeout', function ($parse, $timeout) {
+            return function (scope, element, attr) {
+                $timeout(function () {
+                    var e = angular.element(attr['fadeOnHover']);
+                    if (e.length) {
+                        e.bind('mouseenter', function () {
+                            element.fadeIn('fast');
+                        }).bind('mouseleave', function () {
+                            element.fadeOut('fast');
+                        });
+                    }
+                }, 60);
+                element.hide();
+            }
+        }])
 
-.directive('upload', ['UploadManager', function factory(UploadManager) {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attrs) {
-			element.fileUpload({
-				dataType: 'text',
-				add: function(e, data) {
-					UploadManager.add(data);
-				}
-			});
-		}
-	};
-}])
+        .directive('upload', ['UploadManager', function factory(UploadManager) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.fileUpload({
+                        dataType: 'text',
+                        add: function (e, data) {
+                            UploadManager.add(data);
+                        }
+                    });
+                }
+            };
+        }])
 
+        .directive('fileChange', function () {
+            var linker = function ($scope, element, attributes) {
 
-.directive('fileChange', function() {
-	 var linker = function ($scope, element, attributes) {
-	        // onChange, push the files to $scope.files.
-	        element.bind('change', function (event) {
-	            var files = event.target.files;
-			for (var i = 0, length = files.length; i < length; i++) {
-			    $scope.files.push(files[i]);
-			}
-	        });
-	    };
+                // onChange, push the files to $scope.files.
+                element.bind('change', function (event) {
 
-	    return {
-	        restrict: 'A',
-	        link: linker
-	    };
-});
+                    var files = event.target.files;
 
+                    $scope.$apply(function () {
+                        for (var i = 0, length = files.length; i < length; i++) {
+                            $scope.files.push(files[i]);
+                        }
+                    });
+                });
+            };
 
+            return {
+                restrict: 'A',
+                link: linker
+            };
+        });
 })(angular);
