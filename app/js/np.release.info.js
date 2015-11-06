61aec31aa697fb7aad067ea03978ce8a0141e455
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2015-11-06 12:18:16 +0100

    Update controller to manage statistics categories. Modify display of release tables using bootstrap grid

--- app/js/np.release.info.js
+++ app/js/np.release.info.js
@@ -8,11 +8,41 @@
 
     ReleaseInfoCtrl.$inject = ['$scope','releaseInfoService'];
     function ReleaseInfoCtrl($scope, releaseInfoService) {
-
         releaseInfoService.getReleaseInfo().$promise.then(function(data){
-            $scope.releaseInfo = data;
-        });
 
+            var index = {
+                databaseRelease: [],
+                apiRelease: [],
+                datasources: [],
+                tagStatistics:[]
+            };
+            index.databaseRelease = data.release.databaseRelease;
+            index.apiRelease = data.release.apiRelease;
+
+            _.each(data.release.datasources, function(ds) {
+                index.datasources.push(ds);
+            });
+
+            _.each(data.release.tagStatistics, function(ts) {
+                var stat = {
+                    description: ts.description,
+                    count: ts.count
+                };
+
+                var dbSpecies  = _.find(index.tagStatistics, function(obj) { return obj.category == ts.categroy});
+                if (dbSpecies) {
+                    dbSpecies.data.push(stat);
+                } else {
+                    index.tagStatistics.push({
+                        category: ts.categroy,
+                        data: [stat]
+                    });
+                }
+
+            });
+
+            $scope.releaseInfo = index;
+        });
     }
 
     releaseInfoService.$inject = ['$resource', 'config'];
