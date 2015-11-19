1bb510c29d7ced0f0077f64b3fd6930e8382528c
fnikitin <frederic.nikitin@gmail.com>
2015-11-19 17:12:19 +0100

    Modifying directive

--- app/js/user/np.user.protein.lists.ui.js
+++ app/js/user/np.user.protein.lists.ui.js
@@ -34,24 +34,23 @@
         }])
 
         .directive('fileChange', function () {
-            var linker = function ($scope, element, attributes) {
-
-                // onChange, push the files to $scope.files.
-                element.bind('change', function (event) {
-
-                    var files = event.target.files;
-
-                    $scope.$apply(function () {
-                        for (var i = 0, length = files.length; i < length; i++) {
-                            $scope.files.push(files[i]);
-                        }
-                    });
-                });
-            };
 
             return {
                 restrict: 'A',
-                link: linker
-            };
+                link: function ($scope, element, attributes) {
+
+                    // onChange, push the files to $scope.files list
+                    element.bind('change', function (event) {
+
+                        var files = event.target.files;
+
+                        $scope.$apply(function () {
+                            for (var i = 0, length = files.length; i < length; i++) {
+                                $scope.files.push(files[i]);
+                            }
+                        });
+                    });
+                }
+            }
         });
 })(angular);
