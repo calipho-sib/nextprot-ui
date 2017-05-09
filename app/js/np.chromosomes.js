34acb3bc163645cc17b3e5df7faf7eb065b7a13d
fnikitin <frederic.nikitin@gmail.com>
2017-05-09 14:51:11 +0200

    New module dealing with chromosomal entries

--- app/js/np.chromosomes.js
+++ app/js/np.chromosomes.js
@@ -1,46 +1,33 @@
 (function (angular, undefined) {
     'use strict';
 
-    angular.module('np.chromosomes', ['ngRoute'])
-        .config(chromosomeConfig)
-        .service('chromosomeService', chromosomeService)
+    angular.module('np.chromosomes', [])
+        .factory('chromosomeService', chromosomeService)
         .controller('chromosomeCtrl', chromosomeCtrl);
-
-    chromosomeConfig.$inject = ['$routeProvider'];
-    function chromosomeConfig($routeProvider) {
-
-        console.log("FUCKING YOOOOOOOO")
-
-        $routeProvider.when('/about/entries', {title:'Release Entries', templateUrl: '/partials/doc/entries.html'})
-    }
 
     chromosomeCtrl.$inject = ['$scope', 'chromosomeService'];
     function chromosomeCtrl($scope, chromosomeService) {
 
-        console.log("FUCKING YOOOOOOOO")
-
-        console.log(chromosomeService.getChromosomeNames());
         $scope.chromosomeNames = chromosomeService.getChromosomeNames();
     }
 
     chromosomeService.$inject = ['config', '$http'];
     function chromosomeService(config, $http) {
 
-        console.log("FUCKING YOOOOOOOO", config.api.API_URL + '/chromosome-names.json')
+        var chromosomes = {
+            selected : "",
+            pages : ["all"]
+        };
 
         var ChromosomeService = function() {
-
-            var self = this;
-
-            console.log("FUCKING YOOOOOOOO", config.api.API_URL + '/chromosome-names.json')
 
             $http.get(config.api.API_URL + '/chromosome-names.json')
                 .success(function (response) {
                     console.log("response", response);
-                    self.chromosomes = response;
+                    chromosomes.pages = chromosomes.pages.concat(response);
                 })
                 .error(function (data, status) {
-                    var message = status + ": cannot access list of chromosome names from '" + config.api.API_URL + "/chromosome-names.json'";
+                    var message = status + ": cannot access list of chromosomes from '" + config.api.API_URL + "/chromosome-names.json'";
                     $log.error(message);
                     flash("alert-info", message);
                 });
@@ -48,8 +35,24 @@
 
         ChromosomeService.prototype.getChromosomeNames = function () {
 
-            return this.chromosomes;
+            return chromosomes.pages;
         };
+
+        ChromosomeService.prototype.selectChromosomePage = function (chromosome) {
+
+            if (this.getChromosomeNames().include(chromosome)) {
+                chromosomes.selected = chromosome;
+            }
+            else {
+                chromosomes.selected = "all";
+            }
+        };
+
+        ChromosomeService.prototype.getSelectedPage = function() {
+
+            return chromosomes.selected;
+        };
+
         return new ChromosomeService();
     }
 })(angular); //global variable
