850c0d3eaf32e67928f602355813c18f69a9d850
fnikitin <frederic.nikitin@isb-sib.ch>
2018-01-04 16:26:39 +0100

    prepare next develop version

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.11.0',
+    'version': '2.12.0',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
     'githash': 'GIT_HASH' // 'e3a1a30'
