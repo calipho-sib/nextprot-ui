fc34e94203b01df0df828fd287bfa34ab066fb56
fnikitin <frederic.nikitin@isb-sib.ch>
2018-01-05 13:50:21 +0100

    Adding branch name info in footer

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -4,6 +4,7 @@ angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
     'version': '2.12.0',
+    'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
     'githash': 'GIT_HASH' // 'e3a1a30'
