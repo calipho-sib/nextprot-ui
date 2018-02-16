86e95c86ec4b227b90e201c6aeec39a93b9a3c16
fnikitin <frederic.nikitin@isb-sib.ch>
2018-02-16 14:41:30 +0100

    Fixing release stats and release sources pages

--- app/js/np.release.info.js
+++ app/js/np.release.info.js
@@ -25,70 +25,82 @@
         }
         $scope.releaseInfosFormatted = formatReleaseInfos(RELEASE_INFOS);
 
-        releaseInfoService.getReleaseInfo().$promise.then(function(data){
+        $scope.releaseInfo = {
+            databaseRelease: "",
+            apiRelease: "",
+            datasources: [],
+            tagStatistics: []
+        };
 
-            var index = {
-                databaseRelease: [],
-                apiRelease: [],
-                datasources: [],
-                tagStatistics:[]
-            };
-            index.databaseRelease = data.release.databaseRelease;
-            index.apiRelease = data.release.apiRelease;
+        // fetching release info versions
+        releaseInfoService.getReleaseInfo().$promise.then(function(releaseInfo) {
 
-            _.each(data.release.datasources, function(ds) {
-                index.datasources.push(ds);
+            $scope.releaseInfo.databaseRelease = releaseInfo.versions.databaseRelease;
+            $scope.releaseInfo.apiRelease = releaseInfo.versions.apiRelease;
+        });
+
+        // fetching release data sources
+        releaseInfoService.getReleaseDataSources().$promise.then(function(releaseDataSources) {
+            console.log("releaseDataSources", releaseDataSources.dataSources.datasources);
+
+            _.each(releaseDataSources.dataSources.datasources, function (ds) {
+                $scope.releaseInfo.datasources.push(ds);
             });
+        });
 
-            _.each(data.release.tagStatistics, function(ts) {
+        // fetching release stats
+        releaseInfoService.getReleaseStats().$promise.then(function(data) {
+
+            _.each(data.releaseStats.tagStatistics, function (ts) {
                 var stat = {
                     description: ts.description,
                     count: ts.count
                 };
 
-                var dbSpecies  = _.find(index.tagStatistics, function(obj) { return obj.category == ts.categroy});
+                var dbSpecies = _.find($scope.releaseInfo.tagStatistics, function (obj) {
+                    return obj.category == ts.categroy
+                });
                 if (dbSpecies) {
                     dbSpecies.data.push(stat);
                 } else {
-                    index.tagStatistics.push({
+                    $scope.releaseInfo.tagStatistics.push({
                         category: ts.categroy,
                         data: [stat]
                     });
                 }
 
             });
+        });
 
-            $scope.releaseInfo = index;
 
-            // Replace the publication stats fetched above by the correct ones computed by the new service /publications/stats.json
-            releaseInfoService.getPublicationStats().$promise.then(function(data) {
-                var newStats = [
-                    {
-                        description: "Cited publications",
-                        count: data.numberOfCitedPublications
-                    },
-                    {
-                        description: "Computationally mapped publications",
-                        count: data.numberOfComputationallyMappedPublications
-                    },
-                    {
-                        description: "Large scale publications",
-                        count: data.numberOfLargeScalePublications
-                    },
-                    {
-                        description: "Manually curated publications",
-                        count: data.numberOfCuratedPublications
-                    }
-                ];
-
-                var arrayLength = index.tagStatistics.length;
-                for (var i = 0; i < arrayLength; i++) {
-                    if (index.tagStatistics[i].category === "Publications") {
-                        index.tagStatistics[i].data = newStats;
-                        break;
-                    }
+        // Replace the publication stats fetched above by the correct ones computed by the new service /publications/stats.json
+        releaseInfoService.getPublicationStats().$promise.then(function (data) {
+            var newStats = [
+                {
+                    description: "Cited publications",
+                    count: data.numberOfCitedPublications
+                },
+                {
+                    description: "Computationally mapped publications",
+                    count: data.numberOfComputationallyMappedPublications
+                },
+                {
+                    description: "Large scale publications",
+                    count: data.numberOfLargeScalePublications
+                },
+                {
+                    description: "Manually curated publications",
+                    count: data.numberOfCuratedPublications
                 }
-            });
+            ];
+
+            var arrayLength = $scope.releaseInfo.tagStatistics.length;
+            for (var i = 0; i < arrayLength; i++) {
+                if ($scope.releaseInfo.tagStatistics[i].category === "Publications") {
+                    $scope.releaseInfo.tagStatistics[i].data = newStats;
+                    break;
+                }
+            }
         });
     }
 
@@ -97,6 +109,16 @@
 
         var releaseInfoResource = $resource(
             config.api.API_URL + '/release-info.json',
+            {},
+            {get : {method: "GET"}});
+
+        var releaseStatsResource = $resource(
+            config.api.API_URL + '/release-stats.json',
+            {},
+            {get : {method: "GET"}});
+
+        var releaseDataSources = $resource(
+            config.api.API_URL + '/release-data-sources.json',
             {},
             {get : {method: "GET"}});
 
@@ -111,6 +133,14 @@
 
         ReleaseInfoService.prototype.getReleaseInfo = function () {
             return releaseInfoResource.get();
+        };
+
+        ReleaseInfoService.prototype.getReleaseStats = function () {
+            return releaseStatsResource.get();
+        };
+
+        ReleaseInfoService.prototype.getReleaseDataSources = function () {
+            return releaseDataSources.get();
         };
 
         ReleaseInfoService.prototype.getPublicationStats = function () {
