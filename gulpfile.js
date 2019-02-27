8d8ec2ba586101b992f9863f523d277b6926f78a
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2019-02-27 15:09:18 +0100

    Add copying of colorbar dependency

--- gulpfile.js
+++ gulpfile.js
@@ -29,15 +29,9 @@ gulp.task('copy-elements', function () {
                             .pipe(dest(elementsPath));
     var biovizTask = gulp.src('./bower_components/nextprot-elements/bio-viz-v2/**')
                             .pipe(dest(elementsPath + '/bio-viz-v2'));
-    return [elementsTask, biovizTask];   
-});
-
-/**
- * bio-viz folder has to be in the root web directory, so it has to be copied
- */
-gulp.task('copy-bio-viz', function() {
-    return gulp.src(['./bower_components/nextprot-elements/bio-viz-v2/**'])
-        .pipe(dest(rootPath + '/bio-viz-v2'));
+    var  colorbarTask = gulp.src('./bower_components/nextprot-elements/colorbar/**')
+                            .pipe(dest(elementsPath + '/colorbar'));
+    return [elementsTask, biovizTask, colorbarTask];   
 });
 
 gulp.task('build-elements', function () {
@@ -60,6 +54,14 @@ gulp.task('compress', ['minify-css'] , () => {
     return gulp.src('build/js/*.js')
           .pipe(uglify({ mangle: false }))
           .pipe(gulp.dest('./build/js'));
+});
+
+/**
+ * bio-viz folder has to be in the root web directory, so it has to be copied
+ */
+gulp.task('copy-bio-viz', function() {
+    return gulp.src(['./bower_components/nextprot-elements/bio-viz-v2/**'])
+        .pipe(dest(rootPath + '/bio-viz-v2'));
 });
 
 
