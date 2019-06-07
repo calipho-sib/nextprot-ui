c96f2a2383346a16ab20e81b8091b039b0a7fd3b
Mathieu Schaeffer <mat.schaeff@gmail.com>
2019-06-07 18:04:40 +0200

    fix cloud front issue

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.22.4',
+    'version': '2.22.5',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
