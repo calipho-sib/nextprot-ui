e86bf29a09ee2c86f696e9445017cf3e7b147962
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2019-06-17 22:48:19 +0200

    [PATCH] Blast view pagination fix

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.22.7',
+    'version': '2.22.8',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
