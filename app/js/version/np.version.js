9fba343c346d4a4073748149b9fb06ce3e7d1a1a
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2020-02-27 17:59:10 +0100

    Prepare next develop version

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.26::wq.0',
+    'version': '2.27.0',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
