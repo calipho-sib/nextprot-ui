d09ecb28a213f4a0293552af8d1e0179b4758cf7
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2020-07-31 17:58:02 +0200

    PATCH: logo fixes

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.28.1',
+    'version': '2.28.2',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
