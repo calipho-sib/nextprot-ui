4f8141b705c95fb20c7581910404968567e96fb3
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-03-16 12:45:29 +0100

    Preparing a patch version: Hotfix protein list operation issue

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.33.0',
+    'version': '2.33.1',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
