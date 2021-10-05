02160f7738a8427489f15e433cab19f255132ac0
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-10-05 15:47:33 +0200

    Resolve conflicts and prepare next release version

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,11 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-<<<<<<< Updated upstream
-    'version': '2.38.0',
-=======
     'version': '2.37.1',
->>>>>>> Stashed changes
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
