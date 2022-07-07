8d229494b5f0e396ac467680be1e4f33c1dd9dcc
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2022-07-07 17:56:15 +0200

    Prepare new patch version

--- app/js/version/np.version.js
+++ app/js/version/np.version.js
@@ -3,7 +3,7 @@
 angular.module('np.version', [])
 
 .constant('RELEASE_INFOS', {
-    'version': '2.44.0',
+    'version': '2.44.1',
     'branch': 'BRANCH_NAME',
     "isProduction": 'IS_PRODUCTION', // i.e 'true'
     'build': 'BUILD_NUMBER', // '926'
